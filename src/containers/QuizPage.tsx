56087c499710cd9c1a2f1c4655aa26ec641c5adc
r.hosoya <r.hosoya@core-tech.jp>
2023-05-13 12:40:21 +0900

    リファクタリング完了

--- src/containers/QuizPage.tsx
+++ src/containers/QuizPage.tsx
@@ -1,12 +1,13 @@
 import * as React from "react";
-import { Box, Button, Center, Heading, Text, VStack } from "@chakra-ui/react";
+import { Box, Button, Heading, Text } from "@chakra-ui/react";
 import { useNavigate } from "react-router-dom";
-import { useQuizApi } from "../services/qzinApi";
+import { useQuizApi } from "../services/QuizApi";
 import { Question } from "../components/Quiz/Question";
 import { Choices } from "../components/Quiz/Choices";
 
 export const QuizPage: React.FC = () => {
   const navigate = useNavigate();
+
   const [currentQuestion, setCurrentQuestion] = React.useState(0);
   const [score, setScore] = React.useState(0);
   const [quizFinished, setQuizFinished] = React.useState(false); // 追加
@@ -26,11 +27,14 @@ export const QuizPage: React.FC = () => {
     setQuizFinished(true);
   };
 
-  // 正解時、スコアを増やして次の問題に進む関数
+  // 正解時、スコアを増やして次の問題に進む
+  // クイズが終わると終了画面に遷移する
   const handleAnswer = (isCorrect: Boolean) => {
+    // 正解した時
     if (isCorrect) {
       setScore(score + 1);
     }
+
     if (currentQuestion < questions.length - 1) {
       setCurrentQuestion(currentQuestion + 1);
     } else {
@@ -38,38 +42,38 @@ export const QuizPage: React.FC = () => {
     }
   };
 
+  return (
+    <Box>
+      {/* トップのテキスト */}
+      <Heading as="h2" size="xl" textAlign="center" mt={10}>
+        Quiz
+      </Heading>
 
-return (
-  <Box>
-    <Heading as="h2" size="xl" textAlign="center" mt={10}>
-      Quiz
-    </Heading>
+      {/* 問題を表示する */ }
+      {questions.length > 0 && currentQuestion < questions.length && !quizFinished ? (
+        <>
+          {/* 問題文を表示するコンポーネント */}
+          <Question question={questions[currentQuestion].question} />
 
-    {/* 問題を表示する */}
-    {questions.length > 0 && currentQuestion < questions.length && !quizFinished ? (
-      <>
-        {/* 問題文を表示するコンポーネント */}
-        <Question question={questions[currentQuestion].question} />
-
-        {/* 選択肢を表示するコンポーネント */}
-        <Choices 
-         choices={questions[currentQuestion].choices}
-         correctAnswer={questions[currentQuestion].correctAnswer}
-         onAnswer={handleAnswer}
-         />
-      </>
-    ) : (
-      /* 結果と戻るボタンを表示する */
-      quizFinished && (
-        <Box textAlign="center" mt={8}>
-          <Text fontSize="xl">Quiz finished!</Text>
-          <Text fontSize="xl">Your score: {score}</Text>
-          <Button colorScheme="teal" mt={4} onClick={onBackHome}>
-            Back to Home
-          </Button>
-        </Box>
-      )
-    )}
-  </Box>
- );
+          {/* 選択肢を表示するコンポーネント */}
+          <Choices 
+            choices={questions[currentQuestion].choices}
+            correctAnswer={questions[currentQuestion].correctAnswer}
+            onAnswer={handleAnswer}
+          />
+        </>
+      ) : (
+        /* 結果と戻るボタンを表示する */
+        quizFinished && (
+          <Box textAlign="center" mt={8}>
+            <Text fontSize="xl">Quiz finished!</Text>
+            <Text fontSize="xl">Your score: {score}</Text>
+            <Button colorScheme="teal" mt={4} onClick={onBackHome}>
+              Back to Home
+            </Button>
+          </Box>
+        )
+      )}
+    </Box>
+  );
 };
---
+++ src/services/QuizApi.ts
@@ -0,0 +1,38 @@
+import axios from "axios";
+import React, { useCallback } from "react"
+
+// 型宣言
+type QuizQuestion = {
+	question: string;
+	correctAnswer: string;
+	choices: string[];
+};
+
+// 取得する関数
+export const useQuizApi = () => {
+
+	const [questions, setQuestions] = React.useState<QuizQuestion[]>([]);
+
+	const getQuiz = useCallback(
+		() => {
+			axios
+			.get("https://opentdb.com/api.php?amount=10&difficulty=easy&type=multiple")
+			.then((res) => {
+				const fetchedData = res.data.results.map((question: any) => {
+					const choices = [...question.incorrect_answers, question.correct_answer];
+					const shuffledChoices = choices.sort(() => Math.random() - 0.5);
+					return {
+						question: question.question,
+						correctAnswer: question.correct_answer,
+						choices: shuffledChoices,
+					};
+				});
+				setQuestions(fetchedData);
+			})
+			.catch(() => {
+				alert("データを正常に取得できませんでした(泣)");
+			});
+		}, []);
+
+	return { getQuiz, questions };
+};
